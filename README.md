4d8ad78c6475e210fa56d60cb75cb26ee4da0668
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-17 22:49:14 +0900

    SSR, SSG

--- README.md
+++ README.md
@@ -79,6 +79,29 @@ yarn run dev
 
 <br>
 
+## 📌 SSR vs SSG
+### getServerSideProps 추가 설명
+https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props
+
+- 언제 getServerSideProps를 사용해야 하는가?
+  - request time에 반드시 데이터를 fetch해와야 하는 페이지를 pre-render해야 하는 경우에만 getServerSideProps를 사용해야 한다.
+  - 데이터를 pre-render할 필요가 없다면 client side에서 데이터를 가져오는 것을 고려해야 한다.
+
+- 클라이언트 측에서 데이터 가져오는 과정 (Fetching data on the client side)
+  - 페이지에 자주 업데이트되는 데이터가 포함되어 있고 데이터를 pre-render할 필요가 없는 경우 클라이언트 측에서 데이터를 가져올 수 있다.
+    - 1️⃣ 먼저 데이터가 없는 페이지를 즉시 표시한다.
+    - 2️⃣ 페이지의 일부는 Static Generation을 사용해 pre-render할 수 있다.
+    - 3️⃣ 없는 데이터를 위해 loading 상태를 표시할 수 있다.
+    - 4️⃣ 그런 다음 클라이언트 측에서 데이터를 가져와 준비가 되면 표시한다.
+
+- 이 접근 방식은 예를 들어 사용자 대시보드 페이지에 적합하다.
+  - 왜냐하면 대시보드는 사용자별 비공개 페이지이기 때문에 SEO와는 관련이 없으며 페이지를 미리 렌더링할 필요가 없다. 또한 데이터는 자주 업데이트되므로 요청 시 데이터를 가져와야 합니다.
+
+- getServerSideProps가 오류 페이지를 렌더링하는가?
+  - getServerSideProps 내부에서 오류가 발생하면 pages/500.js 파일이 표시된다.
+  - 500 page(서버 렌더링 오류 페이지)는 사용자가 커스터 마이징 할 수 있다.
+  - 개발 중에는 이 파일이 사용되지 않고 대신 개발 오버레이가 표시된다.
+
 ---
 
 ### 참고 사이트
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
 import Seo from "@/components/Seo";
+import { InferGetServerSidePropsType, GetServerSideProps } from "next";
 
-export interface IMovie {
+export interface MovieProps {
   adult:             boolean;
   backdrop_path:     string;
   genre_ids:         number[];
@@ -18,32 +18,15 @@ export interface IMovie {
   vote_count:        number;
 }
 
-const Home = () => {
-  const [movies, setMovies] = useState<IMovie[]>([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          '/api/movies'
-        );
-        const data = await response.json();
-        setMovies(data.results);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    
-    fetchData();
-  }, []);
-  console.log("movies ", movies)
+const Home = ({ results }: InferGetServerSidePropsType<GetServerSideProps>) => {
+  console.log(results);
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map(movie => (
+      {results?.map((movie: MovieProps) => (
         <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>{movie.title}</h4>
         </div>
       ))}
       <style jsx>{`
@@ -77,3 +60,20 @@ const Home = () => {
 };
 
 export default Home;
+
+// 항상 SSR을 하고 싶은가? - 선택의 문제
+// 1) SSR : loading화면 없이 fetch해온 데이터가 유효할 때 화면이 보여지게 하는 것이 좋은지? getServerSideProps()
+// 하지만 SSR은 fetch API가 느리면 유저에게 아무것도 보이지 않음
+// 2) SSG :또는 static shell 같은 것을 갖고서 유저들에게 loading 화면을 보여준 후에 데이터를 받는게 좋은지? - 아무 설정X이면 SSG가 기본상태
+export const getServerSideProps = async ({ }) => {
+  // api키 넣어도 됨 - 백엔드(서버)에서만 실행되는 함수이므로
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      // 이 props를 _app.tsx의 { pageProps }에 넣어줌! -> Home 컴포넌트의 props처럼 들어오게 되는 것
+      results,
+    },
+  };
+};
