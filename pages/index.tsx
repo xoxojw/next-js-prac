34dd51162715af36502fb3b600f31d2a1bbf3d5b
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-17 14:58:30 +0900

    redirect와 rewrite로 api key 숨기기(fake fetch url)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,77 @@
+import { useEffect, useState } from "react";
 import Seo from "@/components/Seo";
 
+export interface IMovie {
+  adult:             boolean;
+  backdrop_path:     string;
+  genre_ids:         number[];
+  id:                number;
+  original_language: string;
+  original_title:    string;
+  overview:          string;
+  popularity:        number;
+  poster_path:       string;
+  release_date:      Date;
+  title:             string;
+  video:             boolean;
+  vote_average:      number;
+  vote_count:        number;
+}
+
 const Home = () => {
+  const [movies, setMovies] = useState<IMovie[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          '/api/movies'
+        );
+        const data = await response.json();
+        setMovies(data.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    
+    fetchData();
+  }, []);
+  console.log("movies ", movies)
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
-      <h1 className="title">Hello, world!</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map(movie => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie {
+          display: flex;
+          flex-direction: column;
+          margin: 0 auto;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.01) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 };
